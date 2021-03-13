eeb7f38417ee76ede16103c8b576f19052aaa742
Jody <muhammad.adnand.jody@gmail.com>
2021-03-13 10:14:25 +0700

    Unit Testing

--- test/appTest.js
+++ test/appTest.js
@@ -6,7 +6,35 @@ describe('server', function () {
     it('should return 200', function (done) {
         http.get('http://localhost:5000/all', function (res) {
           assert.equal(200, res.statusCode);
-          done();
-        });
+        })
+        done();
+    });
+
+    it('Cats should return 200', function (done) {
+      http.get('http://localhost:5000/cats', function (res) {
+        assert.equal(200, res.statusCode);
+      })
+      done();
+    });
+
+    it('People should return 200', function (done) {
+      http.get('http://localhost:5000/people', function (res) {
+        assert.equal(200, res.statusCode);
+      })
+      done();
+    });
+
+    it('Flowers should return 200', function (done) {
+      http.get('http://localhost:5000/flowers', function (res) {
+        assert.equal(200, res.statusCode);
+      })
+      done();
+    });
+
+    it('Wrong way should return 404', function (done) {
+      http.get('http://localhost:5000/wrong', function (res) {
+        assert.equal(404, res.statusCode);
+      })
+      done();
     });
 });
